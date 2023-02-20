c172fa362ab413c9c019671bef95e17e2de913ea
Kyrylo <kirilltaranskyi@gmail.com>
2023-02-20 15:23:13 +0200

    script.js updated

---
+++ script.js
@@ -0,0 +1,9 @@
+let a = Number(prompt('Введите первое число'));
+let b = Number(prompt('Введите второе число'));
+let c = Number(prompt('Введите третье число'));
+
+let average;
+
+average = (a + b + c) / 3;
+
+alert(`Среднее: ${average}`);
